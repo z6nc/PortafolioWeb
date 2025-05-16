5bc26a8240a3e4b3aa58cddbd5d85d095d9c447f
z6nc <andresvelasquezarotinco@gmail.com>
2025-05-16 12:41:00 -0500

    add : nuevos proyectos en la data

--- src/components/ListProyect.js
+++ src/components/ListProyect.js
@@ -1,55 +1,78 @@
 export const proyectos =
-[
-    {
-        id: 1,
-        nombre: "EnergiX",
-        descripcionCorta: "Tienda de bebidas online Creada con React , Astro , talwindcss , cloudinary e imagenes generadas con IA."  ,
-        descripcionLarga: "Energix es una web creada para mostrar bebidas de diversas categorías, ofreciendo una experiencia sencilla e intuitiva. Todas las imágenes fueron generadas con inteligencia artificial, dando un toque creativo al proyecto. Es una plataforma desarrollada desde cero con un enfoque práctico y funcional.",
-        tecnologias: ["Astro", "React", "TailwindCSS"],
-        webSite: "https://web-drinks-shop.vercel.app",
-        LinkCode: "https://github.com/z6nc/Web-DrinksShop",
-        imagenPortada:"https://res.cloudinary.com/dleem7osr/image/upload/v1746833428/portadaDrink_madbqb.webp",
-        videoWebm:"https://res.cloudinary.com/dleem7osr/video/upload/v1734622001/VideoEnergix_dhdpln.mp4",
+    [
+        {
+            id: 1,
+            nombre: "EnergiX",
+            descripcionCorta: "Tienda de bebidas online Creada con React , Astro , talwindcss , cloudinary e imagenes generadas con IA.",
+            descripcionLarga: "Energix es una web creada para mostrar bebidas de diversas categorías, ofreciendo una experiencia sencilla e intuitiva. Todas las imágenes fueron generadas con inteligencia artificial, dando un toque creativo al proyecto. Es una plataforma desarrollada desde cero con un enfoque práctico y funcional.",
+            tecnologias: ["Astro", "React", "TailwindCSS"],
+            webSite: "https://web-drinks-shop.vercel.app",
+            LinkCode: "https://github.com/z6nc/Web-DrinksShop",
+            imagenPortada: "https://res.cloudinary.com/dleem7osr/image/upload/v1746833428/portadaDrink_madbqb.webp",
+            videoWebm: "https://res.cloudinary.com/dleem7osr/video/upload/v1734622001/VideoEnergix_dhdpln.mp4",
 
-    },
-    
-    {
-        id: 2,
-        nombre: "LaboIdeas",
-        descripcionCorta: "Plataforma de miniproyectos desarrollada con JavaScript.",
-        descripcionLarga: "Laboratorio de Ideas es una plataforma web creada para desarrollar miniproyectos prácticos y rápidos en JavaScript. Actualmente, ofrece 5 proyectos destacados, como BlackJack, CalcuPropina y TestimonyCard, entre otros, cada uno acompañado de su descripción detallada y código fuente.",
-        tecnologias: ["Astro", "JavaScript", "TailwindCSS"],
-        webSite: "https://lab-inspira.vercel.app",
-        LinkCode: "https://github.com/z6nc/LabInspira",
-        imagenPortada: "https://res.cloudinary.com/dleem7osr/image/upload/v1746833428/portadaLABO_cvrpbx.webp",
-        videoWebm: "https://res.cloudinary.com/dleem7osr/video/upload/v1733164116/20241202-1826-54.7084419_qhwbhv.mp4",
-     
-    },
-    
-    {
-        id: 3,
-        nombre: "Codemaster JS ",
-        descripcionCorta: "Plataforma educativa para aprender JavaScript de forma práctica con ejercicios y soluciones.",
-        descripcionLarga: "CodeMasterJS es una plataforma educativa diseñada para aprender JavaScript de forma práctica mediante desafíos y problemas, esta plataforma destaca por ofrecer múltiples soluciones para cada problema, demostrando que en la programación existen diversas maneras de abordar un desafío.",
-        tecnologias: ["Astro", "JavaScript", "TailwindCSS"],
-        webSite: "https://code-master-js.vercel.app",
-        LinkCode: "https://github.com/z6nc/CodeMasterJS",
-        imagenPortada: "https://res.cloudinary.com/dleem7osr/image/upload/v1746833428/portadaJS_ojwysf.webp",
-        videoWebm :"https://res.cloudinary.com/dleem7osr/video/upload/v1733163556/20241202-1817-39.8365737_zrt77x.mp4",
- 
-        
-    },
-    {
-        id: 4,
-        nombre: "WeatherNess ",
-        descripcionCorta: "Una aplicación web de clima en tiempo real, desarrollada con Node.js y Express en el backend ",
-        descripcionLarga: "Aplicación web que muestra información meteorológica en tiempo real utilizando una API de clima. Desarrollada con Node.js y Express en el backend, y un frontend dinámico. Obtiene datos como temperatura, condiciones climáticas y pronósticos, presentados de forma clara y moderna.",
-        tecnologias: ["NodeJS", "Express", "HTML", "CSS", "JavaScript"],
-        webSite: "https://github.com/z6nc/WeatherNess",
-        LinkCode: "https://github.com/z6nc/WeatherNess",
-        imagenPortada: "https://res.cloudinary.com/dleem7osr/image/upload/v1746982855/PortadaWeather_o8uebk.webp",
-        videoWebm :"https://res.cloudinary.com/dleem7osr/video/upload/v1740675379/videoWeatherApp_ncqbj8.mp4",
-    }
-    
+        },
 
-]
+        {
+            id: 2,
+            nombre: "LaboIdeas",
+            descripcionCorta: "Plataforma de miniproyectos desarrollada con JavaScript.",
+            descripcionLarga: "Laboratorio de Ideas es una plataforma web creada para desarrollar miniproyectos prácticos y rápidos en JavaScript. Actualmente, ofrece 5 proyectos destacados, como BlackJack, CalcuPropina y TestimonyCard, entre otros, cada uno acompañado de su descripción detallada y código fuente.",
+            tecnologias: ["Astro", "JavaScript", "TailwindCSS"],
+            webSite: "https://lab-inspira.vercel.app",
+            LinkCode: "https://github.com/z6nc/LabInspira",
+            imagenPortada: "https://res.cloudinary.com/dleem7osr/image/upload/v1746833428/portadaLABO_cvrpbx.webp",
+            videoWebm: "https://res.cloudinary.com/dleem7osr/video/upload/v1733164116/20241202-1826-54.7084419_qhwbhv.mp4",
+
+        },
+
+        {
+            id: 3,
+            nombre: "Codemaster JS ",
+            descripcionCorta: "Plataforma educativa para aprender JavaScript de forma práctica con ejercicios y soluciones.",
+            descripcionLarga: "CodeMasterJS es una plataforma educativa diseñada para aprender JavaScript de forma práctica mediante desafíos y problemas, esta plataforma destaca por ofrecer múltiples soluciones para cada problema, demostrando que en la programación existen diversas maneras de abordar un desafío.",
+            tecnologias: ["Astro", "JavaScript", "TailwindCSS"],
+            webSite: "https://code-master-js.vercel.app",
+            LinkCode: "https://github.com/z6nc/CodeMasterJS",
+            imagenPortada: "https://res.cloudinary.com/dleem7osr/image/upload/v1746833428/portadaJS_ojwysf.webp",
+            videoWebm: "https://res.cloudinary.com/dleem7osr/video/upload/v1733163556/20241202-1817-39.8365737_zrt77x.mp4",
+
+
+        },
+        {
+            id: 4,
+            nombre: "WeatherNess ",
+            descripcionCorta: "Una aplicación web de clima en tiempo real, desarrollada con Node.js y Express en el backend ",
+            descripcionLarga: "Aplicación web que muestra información meteorológica en tiempo real utilizando una API de clima. Desarrollada con Node.js y Express en el backend, y un frontend dinámico. Obtiene datos como temperatura, condiciones climáticas y pronósticos, presentados de forma clara y moderna.",
+            tecnologias: ["NodeJS", "Express", "HTML", "CSS", "JavaScript"],
+            webSite: "https://github.com/z6nc/WeatherNess",
+            LinkCode: "https://github.com/z6nc/WeatherNess",
+            imagenPortada: "https://res.cloudinary.com/dleem7osr/image/upload/v1746982855/PortadaWeather_o8uebk.webp",
+            videoWebm: "https://res.cloudinary.com/dleem7osr/video/upload/v1740675379/videoWeatherApp_ncqbj8.mp4",
+        },
+        {
+            id: 5,
+            nombre: "Rimayninchik",
+            descripcionCorta: "Plataforma web interactiva para aprender quechua con lecciones modulares, audio nativo y traducción por IA. ",
+            descripcionLarga: "Rimayninchik es un proyecto personal de una plataforma web educativa diseñada para facilitar el aprendizaje del quechua. Incluye lecciones modulares, traducción en tiempo real mediante la API de inteligencia artificial Magic Loop, y componentes interactivos que integran audio nativo para reforzar la pronunciación. El diseño es completamente responsive y accesible, adaptándose a cualquier dispositivo. Desarrollado con React, Tailwind CSS, Web Components y React Router, este proyecto fortaleció mis habilidades en frontend, gestión de estado y consumo de APIs",
+            tecnologias: ["React", "TailwindCSS", "JavaScript"],
+            webSite: "https://github.com/z6nc/AprendeQuechua",
+            LinkCode: "https://github.com/z6nc/AprendeQuechua",
+            imagenPortada: "https://res.cloudinary.com/dleem7osr/image/upload/v1747413631/PortadaQuechua_rssdpc.webp",
+            videoWebm: "https://res.cloudinary.com/dleem7osr/video/upload/v1740675379/videoquechuaWeb_gpvnba.mp4  ",
+        },
+        {
+            id: 6,
+            nombre: "VREPLAY",
+            descripcionCorta: "Plataforma web que centraliza torneos y partidas del tier 1 de Valorant, desarrollada con TypeScript, React y Tailwind CSS",
+            descripcionLarga: "VREPLAY es una plataforma web que recopila y organiza todos los torneos y partidas del nivel competitivo tier 1 del videojuego Valorant. Desarrollada con React y TypeScript, utiliza Tailwind CSS para el diseño, y emplea estructuras de datos eficientes, validaciones tipadas y manejo avanzado de estado. También implementé portales para la gestión dinámica de componentes. Este proyecto me permitió afianzar conocimientos sólidos en desarrollo frontend tipado",
+            tecnologias: ["React","TypeScript" ,"TailwindCSS"],
+            webSite: "https://github.com/z6nc/WeatherNess",
+            LinkCode: "https://github.com/z6nc/WeatherNess",
+            imagenPortada: "https://res.cloudinary.com/dleem7osr/image/upload/v1747415703/PortadaVREPLAY01_clgmcs.webp",
+            videoWebm: "https://res.cloudinary.com/dleem7osr/video/upload/v1740675379/videovreplay01_bhoeju.mp4",
+        },
+
+
+
+    ]
