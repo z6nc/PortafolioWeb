12525cd1a3858259be180d7bb4867efc63e1e236
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-28 20:59:01 -0500

    fix : eliminar fragmento innecesario y ajustar límite en el componente CertificacionesFilter

--- src/components/Certificaciones/cetificacionLista.jsx
+++ src/components/Certificaciones/cetificacionLista.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
 import { certifications } from "./certificaciones";
 import { ComCertificado } from "./pageCerti";
 export function CertificacionesFilter({ empresa }) {
   const filterItem = certifications.filter(
     (certificacion) => certificacion.company === empresa
   );
+
+
   return (
-    <>
       <section className="w-full  ">
         <div class="flex items-center justify-center mb-4 py-9 ">
           <span class="flex-grow border-t-1 border-white/25"></span>
           <h2 class="text-xl uppercase text-white font-bold mx-7">{empresa}</h2>
           <span class="flex-grow border-t-1 border-white/25"></span>
         </div>
-        <ComCertificado Certificaciones={filterItem} limit={9} />
+        <ComCertificado Certificaciones={filterItem}  />
       </section>
-    </>
   );
 }
