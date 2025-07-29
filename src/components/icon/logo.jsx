4a87f66d6714626ea1a8a2fc2e8f7d62a595a7d1
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-28 20:58:32 -0500

    fix : agregar atributo loading a la imagen en el componente Logo

--- src/components/icon/logo.jsx
+++ src/components/icon/logo.jsx
@@ -3,5 +3,6 @@ export const Logo = ({styleLogo}) => (
       className={styleLogo}
       src="https://res.cloudinary.com/dleem7osr/image/upload/v1736990452/LogoAndres_en6qhb.png"
       alt="Imagen de mi logo"
+      loading="eager"
     />
     )
