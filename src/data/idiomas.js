a0547ac3eb7086d057b47d40cb2c9c4f94615766
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-28 20:58:21 -0500

    add : crear archivo idiomas.js con datos de idiomas y sus imágenes

---
+++ src/data/idiomas.js
@@ -0,0 +1,12 @@
+export const Idiomas =[
+{
+    nombre : "Español",
+    PaisImg :"https://cloudfront-us-east-1.images.arcpublishing.com/infobae/3VWSU4BLXNBNTD4GHZIJR53XWI.jpg",
+    nivel :"Nativo",
+},
+{
+    nombre : "Ingles",
+    PaisImg :"https://banderas.top/wp-content/uploads/2020/07/USA-flag-28-estados.png",
+    nivel :"Intermedio"
+}
+]
