c70eff22d2c5437988682227cda0457f822b8c08
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-28 20:57:33 -0500

    fix : mejora la estructura y el formato del componente PageCarga

--- src/components/pageCarga.jsx
+++ src/components/pageCarga.jsx
@@ -1,13 +1,12 @@
 import { Logo } from "./icon/logo";
 export function PageCarga() {
   return (
-    <div className="  w-full  flex  flex-col justify-center items-center ">
-      <div className="min-h-screen ">
-        <Logo styleLogo=" animate-pulse" />
-      
-      <p className="text-white text-center text-3xl  font-black animate-pulse ">
-        CARGANDO
-      </p>
+    <div className="w-full min-h-screen">
+      <div className="flex flex-col justify-center items-center">
+        <Logo styleLogo="animate-pulse " />
+        <p className="text-white text-center text-3xl  font-black animate-pulse ">
+          CARGANDO ...
+        </p>
       </div>
     </div>
   );
